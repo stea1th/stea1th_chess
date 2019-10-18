7291d15cb5b1307dc7305c1491ecb08e9e6d1220
Vadim Vlasov <stea1th@mail.ru>
2019-10-18 15:16:12 +0200

    pom + chessboard

--- src/main/java/stea1th/chess/ChessBoard.java
+++ src/main/java/stea1th/chess/ChessBoard.java
@@ -2,7 +2,40 @@ package stea1th.chess;
 
 public class ChessBoard {
 
+    private final static int BOARD_SIZE = 8;
 
 
+    public static void printBoard() {
+        String white = "   ";
+        String black = "===";
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            System.out.println("");
+            System.out.println("---------------------------------");
+            for (int column = 1; column <= BOARD_SIZE; column++) {
+                int num = column + BOARD_SIZE * row;
+                if (row % 2 == 0) {
+                    System.out.print("|" + (num % 2 == 1 ? white : black));
+                } else {
+                    System.out.print("|" + (num % 2 == 1 ? black : white));
+                }
+            }
+            System.out.print("|");
+        }
+        System.out.println("");
+        System.out.println("---------------------------------");
+    }
 
+    public static void printNumbers() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            System.out.println("");
+            System.out.println("---------------------------------");
+            for (int column = 1; column <= BOARD_SIZE; column++) {
+                String num = "" + (column + BOARD_SIZE * row);
+                System.out.print("| " + (num.length() == 1 ? num + " " : num));
+            }
+            System.out.print("|");
+        }
+        System.out.println("");
+        System.out.println("---------------------------------");
+    }
 }
--- src/main/java/stea1th/chess/Main.java
+++ src/main/java/stea1th/chess/Main.java
@@ -3,6 +3,8 @@ package stea1th.chess;
 public class Main {
 
     public static void main(String[] args) {
+        ChessBoard.printBoard();
+        ChessBoard.printNumbers();
 
     }
 }
